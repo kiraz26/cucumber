92ad4bf5a58d1d70c96073144a3d56d115a3ee05
kiraz26 <halilkirazz@gmail.com>
2018-03-17 14:47:31 -0400

    nbr?

---
+++ first-cucumber-project/src/test/java/tesla/HomePageStepDefinitions.java
@@ -0,0 +1,26 @@
+package tesla;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HomePageStepDefinitions {
+	@Given("^I am on the home page$")
+	public void i_am_on_the_home_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^I click on the model S link$")
+	public void i_click_on_the_model_S_link() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Model S home page should be displayed$")
+	public void model_S_home_page_should_be_displayed() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+}
